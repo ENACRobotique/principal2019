12125caba081235f4a780e2c725f9d26b7892886
ElieGrnr <elie.grenier@gmail.com>
2019-02-18 20:03:51 +0100

    [Path Tracking] Starts a little code. Pure Pursuit

--- Principal2019Code/pure_pursuit.py
+++ Principal2019Code/pure_pursuit.py
@@ -9,19 +9,38 @@ class Point:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+        
+    def __repr__(self):
+        print("x = : ", self.x, "y = : ", self.y, "\n")
 
-class RobotState:
+class Robot:
 
     def __init__(self, x, y, theta, v, omega):
         self.x = x
         self.y = y
-        self.theta = thetat
+        self.theta = theta
         self.v = v
         self.omega = omega
-
-class Path:
-    
-    def __init__(self):
-        self.path = np.array([])
         
+    def update(self, x, y, theta, v, omega):
+        self.x = x
+        self.y = y
+        self.theta = theta
+        self.v = v
+        self.omega = omega
+        
+    def set_path(self, path):
+        self.path = path
+        
+    def __repr__(self):
+        print("robot : ", self.x, self.y, self.theta, self.v, self.omega, "\n")
+        
+
+        #self.goal = Point(0,0)
+        
+    def calculate_goal_point(self, robot):
+        dx = [robot.x - point.x for point in self.path]     
+        dy = [robot.y - point.y for point in self.path] 
+        d = [sqrt(dx**2 
+    
      
