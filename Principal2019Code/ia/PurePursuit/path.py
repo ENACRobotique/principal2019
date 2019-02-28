efac568f61979ca3f00335f3baff416ad779b6f3
ElieGrnr <elie.grenier@gmail.com>
2019-02-28 21:06:15 +0100

    [PurePursuit] Debug. Removes unnecessary Robot class.

--- Principal2019Code/ia/PurePursuit/path.py
+++ Principal2019Code/ia/PurePursuit/path.py
@@ -31,7 +31,7 @@ class Path:
     def __init__(self, points):
         self.points = points  #array of points from class Point
         self.headings = self.compute_headings()
-        self.dists = self.compute_dists
+        self.dists = self.compute_dist()
         self.last_passed_index = 0
         """if len(headings) > 0:
             self.headings = headings
@@ -54,8 +54,10 @@ class Path:
                 self.headings[i] = np.arctan2(dp.y, dp.x)   
                            
     def compute_dist(self):
-        self.dists = np.zeros(len(self.points))
-        for i, _ in enumerate(self.points):
+        n = len(self.points)
+        self.dists = np.zeros(n)
+        self.dists[0] = 0
+        for i in range(0, n-1):
             self.dists[i+1] = self.dists[i] + dist(self.points[i+1], self.points[i])
 
     def find_closest_point(self, p0):
@@ -65,11 +67,11 @@ class Path:
 
     def find_goal_point(self, p0, look_ahead_distance):
         index_start = self.find_closest_point(p0)
-        p_start = self.path[index_start]
+        p_start = self.points[index_start]
         index = index_start
         dist_to_p0 = dist(p0, p_start)
-        path_length = len(self.path)
-        while index < path_length and len(dist_to_p0) < look_ahead_distance:
+        path_length = len(self.points)
+        while index < path_length and dist_to_p0 < look_ahead_distance:
             index += 1
-            dist_to_p0 = dist(p0, self.path[index])
+            dist_to_p0 = dist(p0, self.points[index])
         return index, self.points[index]
