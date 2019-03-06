7f9c1923c89c2715be2c677992d9c0467eb2bd7f
ElieGrnr <elie.grenier@gmail.com>
2019-03-06 17:41:19 +0100

    [PurePursuit] Computes curvatures.

--- Principal2019Code/ia/PurePursuit/path.py
+++ Principal2019Code/ia/PurePursuit/path.py
@@ -30,7 +30,7 @@ class Path:
 
     def __init__(self, points):
         self.points = points  #array of points from class Point
-        self.headings = self.compute_headings()
+        #self.headings = self.compute_headings()
         self.dists = self.compute_dist()
         self.last_passed_index = 0
         """if len(headings) > 0:
@@ -59,6 +59,30 @@ class Path:
         self.dists[0] = 0
         for i in range(0, n-1):
             self.dists[i+1] = self.dists[i] + dist(self.points[i+1], self.points[i])
+
+    def compute_curvature(self):
+        self.curvature = np.zeros(len(self.points))
+        for i in range(1, len(self.points) - 1):
+            (x1, y1) = self.points[i-1].x, self.points[i-1].y
+            (x2, y2) = self.points[i].x, self.points[i].y
+            (x3, y3) = self.points[i+1].x, self.points[i+1].y
+            d1, d2, d3 = x1**2+y1**2, x2**2+y2**2, x3**2+y3**2
+            A = np.linalg.det(np.array([[x1, y1, 1], [x2, y2, 1], [x3, y3, 1]]))
+            B = -np.linalg.det(np.array([[d1, y1, 1], [d2, y2, 1], [d3, y3, 1]]))
+            C = np.linalg.det(np.array([[d1, x1, 1], [d2, x2, 1], [d3, x3, 1]]))
+            D = -np.linalg.det(np.array([[d1, x1, y1], [d2, x2, y2], [d3, x3, y3]]))
+            if abs(A) < 1e-9:
+                self.curvature[i] = 0.
+            else:
+                curve = (A ** 2)/(0.25*(B ** 2 + C ** 2 - 4 * A * D))
+                curve = np.sqrt(curve)
+                self.curvature[i] = curve
+                a, b = self.points[i].x-self.points[i-1].x, self.points[i].y-self.points[i-1].y
+                c, d = self.points[i+1].x-self.points[i-1].x, self.points[i+1].y-self.points[i-1].y
+                cross = np.linalg.det([[a, c],[b, d]])
+                self.curvature[i] *= np.sign(cross)
+        self.curvature[0] = self.curvature[1]
+        self.curvature[-1] = self.curvature[-2]
 
     def find_closest_point(self, p0, max_index=100):
         dist_to_p0 = np.array([dist(p0, p) for p in self.points[self.last_passed_index:self.last_passed_index+max_index]])
