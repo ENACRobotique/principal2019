55322797c4e5b64f50d3a62047a892f42a05a7ba
ElieGrnr <elie.grenier@gmail.com>
2019-03-06 11:38:56 +0100

    [PurePursuit] Corrects formula

--- Principal2019Code/ia/PurePursuit/pure_pursuit.py
+++ Principal2019Code/ia/PurePursuit/pure_pursuit.py
@@ -32,10 +32,13 @@ class PurePursuit:
     def __init__(self, path):
         self.path = path
 
-    def compute(self, current_robot, look_ahead_distance):
+    def compute(self, current_robot, look_ahead_distance, loop=False):
         p_robot = Point(current_robot.x, current_robot.y)
         theta = current_robot.theta
-        index, p_goal = self.path.find_goal_point(p_robot, look_ahead_distance)
-        x_body = cos(theta)*p_goal.x - sin(theta)*p_goal.y - p_robot.x
+        if not loop:
+            index, p_goal = self.path.find_goal_point(p_robot, look_ahead_distance)
+        else:
+            index, p_goal = self.path.find_goal_point_loop(p_robot, look_ahead_distance)
+        x_body = -sin(theta)*(p_goal.x-p_robot.x) + cos(theta)*(p_goal.y-p_robot.y)
         omega_control = ((2*current_robot.speed)/(look_ahead_distance**2))*x_body
         return omega_control
